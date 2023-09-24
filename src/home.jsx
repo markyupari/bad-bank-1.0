42fe3e0960f043f00db53511f320503c8119216c
markyupari <130168702+markyupari@users.noreply.github.com>
2023-09-24 14:24:59 -0500

    fix image error 404

--- src/home.jsx
+++ src/home.jsx
@@ -22,7 +22,7 @@ function Home() {
               <Card.Img
                 variant="top"
                 className="img-fluid"
-                src="/public/logo.png"
+                src="/logo.png"
                 alt="Bank Logo image"
               />
             }
